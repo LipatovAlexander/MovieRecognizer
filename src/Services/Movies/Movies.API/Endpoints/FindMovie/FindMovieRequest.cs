0927c7f80f08a1e311933af9f6abd8e01294b7d0
LipatovAlexander <lipatov.work@bk.ru>
2023-12-04 13:03:20 +0300

    feat: Movies API

---
+++ src/Services/Movies/Movies.API/Endpoints/FindMovie/FindMovieRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Movies.API.Endpoints.FindMovie;
+
+public sealed class FindMovieRequest
+{
+    public required string Title { get; set; }
+}
---
+++ tests/Movies.IntegrationTests/CommonTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Movies.IntegrationTests;
+
+public sealed class CommonTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+{
+    [Fact]
+    public async Task ShouldReturnNotFound_WhenEndpointNotFound()
+    {
+        // Arrange
+        var client = factory.CreateClient();
+
+        // Act
+        var httpResponse = await client.GetAsync("/not_found");
+
+        // Assert
+        httpResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        var apiResponse = await httpResponse.Content.ReadFromJsonAsync<ApiResponse>();
+        apiResponse.Should().NotBeNull();
+        apiResponse?.Ok.Should().BeFalse();
+        apiResponse?.Code.Should().Be("not_found");
+    }
+    
+    [Fact]
+    public async Task ShouldReturnInternalError_WhenErrorOccurred()
+    {
+        // Arrange
+        var client = factory.CreateClient();
+
+        // Act
+        var httpResponse = await client.GetAsync("/boom");
+
+        // Assert
+        httpResponse.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+
+        var apiResponse = await httpResponse.Content.ReadFromJsonAsync<ApiResponse>();
+        apiResponse.Should().NotBeNull();
+        apiResponse?.Ok.Should().BeFalse();
+        apiResponse?.Code.Should().Be("internal_error");
+    }
+}
