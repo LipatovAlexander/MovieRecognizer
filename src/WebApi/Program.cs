afdb47bc4d5d90b77adaf4b60c02484d493940b0
Alexander Lipatov <lipatov@mindbox.cloud>
2024-02-05 00:08:38 +0300

    feat: Hangfire client

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,30 +1,45 @@
 using System.Text.Json.Serialization;
 using Application;
 using Domain;
+using Hangfire;
+using Hangfire.PostgreSql;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var services = builder.Services;
+var configuration = builder.Configuration;
+
 builder.AddServiceDefaults();
 
 builder.AddNpgsqlDbContext<ApplicationDbContext>("application");
-builder.Services.AddScoped<IApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
+services.AddScoped<IApplicationDbContext>(sp => sp.GetRequiredService<ApplicationDbContext>());
 
-builder.Services.ConfigureHttpJsonOptions(options =>
+services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+services.AddHangfire(config =>
+{
+    config.UsePostgreSqlStorage(c =>
+    {
+        c.UseNpgsqlConnection(configuration.GetConnectionString("hangfire"));
+    });
 });
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-app.MapPost("recognize", async (Uri videoUrl, IApplicationDbContext dbContext) =>
+app.MapPost("recognize", async (Uri videoUrl, IApplicationDbContext dbContext, IBackgroundJobClient backgroundJob) =>
 {
     var request = new RecognitionRequest(videoUrl, DateTimeOffset.UtcNow, RecognitionRequestStatus.Created);
     dbContext.RecognitionRequests.Add(request);
     await dbContext.SaveChangesAsync();
+
+    backgroundJob.Enqueue(() => Console.WriteLine($"Request {request.Id} created"));
 
     return Results.Created("recognize", new { id = request.Id });
 });
