f7d1ae8d8697f5b51d716b17a8397d151071e867
Alexander Lipatov <lipatov@mindbox.cloud>
2024-02-08 22:46:09 +0300

    refactor: Configure enum to string conversions globally

--- src/Infrastructure/ApplicationDbContext.cs
+++ src/Infrastructure/ApplicationDbContext.cs
@@ -10,7 +10,11 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<Enum>().HaveConversion<string>();
     }
 }
