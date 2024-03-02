2d72522982adf380d52c09a4f9461e3a063c4a2a
Alexander <77479370+LipatovAlexander@users.noreply.github.com>
2024-03-02 22:33:03 +0300

    Create service account for image search (#13)

--- terraform/yandex-reverse-image-search.tf
+++ terraform/yandex-reverse-image-search.tf
@@ -15,3 +15,21 @@ resource "yandex_function" "yandex-reverse-image-search" {
     zip_filename = data.archive_file.yandex-reverse-image-search.output_path
   }
 }
+
+resource "yandex_iam_service_account" "sa-yandex-reverse-image-search" {
+  name        = "yandex-reverse-image-search-sa"
+  description = "Service account to invoke yandex-reverse-image-search"
+}
+
+resource "yandex_iam_service_account_api_key" "sa-yandex-reverse-image-search-api-key" {
+  service_account_id = yandex_iam_service_account.sa-yandex-reverse-image-search.id
+}
+
+resource "yandex_function_iam_binding" "yandex-reverse-image-search-iam" {
+  function_id = yandex_function.yandex-reverse-image-search.id
+  role        = "functions.functionInvoker"
+
+  members = [
+    "serviceAccount:${yandex_iam_service_account.sa-yandex-reverse-image-search.id}",
+  ]
+}
