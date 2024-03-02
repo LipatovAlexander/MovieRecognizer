ae1226a4967da013ce40205e87065f6fd486ebc4
Alexander Lipatov <lipatov@mindbox.cloud>
2024-03-02 18:49:35 +0300

    ci: terraform

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "movie-recognizer-terraform-state"
+    region = "ru-central1"
+    key    = "main.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+}
+
+provider "yandex" {
+  zone = "ru-central1-a"
+}
