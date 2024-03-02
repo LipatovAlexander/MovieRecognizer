1bbfa2ef128ce2769cf678f819d192c7d3272ea3
Alexander <77479370+LipatovAlexander@users.noreply.github.com>
2024-03-02 19:56:14 +0300

    Create s3 bucket (#6)
    * Create s3 bucket
    
    * fix

---
+++ terraform/bucket.tf
@@ -0,0 +1,24 @@
+locals {
+  bucket_name = "movie-recognizer"
+}
+
+resource "yandex_storage_bucket" "bucket" {
+  bucket = local.bucket_name
+  access_key = var.deployer_access_key
+  secret_key = var.deployer_secret_key
+
+  grant {
+    id          = yandex_iam_service_account.sa_bucket.id
+    type        = "CanonicalUser"
+    permissions = ["READ", "WRITE"]
+  }
+}
+
+resource "yandex_iam_service_account" "sa_bucket" {
+  name        = "${local.bucket_name}-sa"
+  description = "Service account to manage ${local.bucket_name}"
+}
+
+resource "yandex_iam_service_account_static_access_key" "sa_key_bucket" {
+  service_account_id = yandex_iam_service_account.sa_bucket.id
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable deployer_access_key {
+    type = string
+    sensitive = true
+}
+
+variable deployer_secret_key {
+    type = string
+    sensitive = true
+}
