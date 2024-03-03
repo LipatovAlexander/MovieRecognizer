8079b3314e482d6c163f5263639d7370e3c0316e
Alexander Lipatov <lipatov@mindbox.cloud>
2024-03-03 23:59:27 +0300

    Create queue trigger

--- terraform/receive-video-handler.tf
+++ terraform/receive-video-handler.tf
@@ -36,3 +36,32 @@ resource "yandex_message_queue" "receive-video-handler-deadletter-queue" {
   access_key = var.deployer_access_key
   secret_key = var.deployer_secret_key
 }
+
+resource "yandex_function_trigger" "receive-video-handler-trigger" {
+  name        = "receive-video-handler-trigger"
+  message_queue {
+    queue_id           = yandex_message_queue.receive-video-handler-queue.id
+    service_account_id = yandex_iam_service_account.receive-video-handler-trigger-sa.id
+    batch_size         = "1"
+    batch_cutoff       = "0"
+  }
+  function {
+    id = yandex_function.receive-video-handler.id
+  }
+}
+
+resource "yandex_iam_service_account" "receive-video-handler-trigger-sa" {
+  name        = "receive-video-handler-sa"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "receive-video-handler-trigger-editor" {
+  folder_id   = var.folder_id
+  role        = "editor"
+  member      = "serviceAccount:${yandex_iam_service_account.receive-video-handler-trigger-sa.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "receive-video-handler-function-trigger-invoker" {
+  folder_id   = var.folder_id
+  role        = "functions.functionInvoker"
+  member      = "serviceAccount:${yandex_iam_service_account.receive-video-handler-trigger-sa.id}"
+}
