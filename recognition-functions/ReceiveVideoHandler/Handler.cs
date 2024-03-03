43348504b20017a95b931856ded35dca1d64a1c2
Alexander Lipatov <lipatov@mindbox.cloud>
2024-03-03 23:29:25 +0300

    refactor: Function

--- recognition-functions/ReceiveVideoHandler/Handler.cs
+++ recognition-functions/ReceiveVideoHandler/Handler.cs
@@ -4,7 +4,7 @@ public class Handler
 {
     public async Task<Response> FunctionHandler(Request request)
     {
-        Console.WriteLine(request.body);
-        return new Response(200, "Hello, world!");
+        Console.WriteLine(request.Body);
+        return new Response(200, "Hello, world!", "application/json");
     }
 }
