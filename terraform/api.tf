f38ce23255aac43859060623c1bf3d3f1ed741af
Alexander Lipatov <lipatov@mindbox.cloud>
2024-03-04 22:06:57 +0300

    fix

--- terraform/api.tf
+++ terraform/api.tf
@@ -2,6 +2,15 @@ resource "yandex_iam_service_account" "api-sa" {
   name        = "api-sa"
 }
 
+resource "yandex_container_registry_iam_binding" "api-sa-puller" {
+  registry_id = var.api_registry
+  role        = "container-registry.images.puller"
+
+  members = [
+    "serviceAccount:${yandex_iam_service_account.api-sa.id}",
+  ]
+}
+
 resource "yandex_serverless_container" "test-container" {
   name               = "api"
   memory             = 128
