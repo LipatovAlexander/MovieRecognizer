b680924e5b81cf431ce1d268c72342f9a86a04fb
Alexander <77479370+LipatovAlexander@users.noreply.github.com>
2024-03-10 21:47:43 +0300

    Create movie-recognition YDB (#34)

---
+++ terraform/ydb.tf
@@ -0,0 +1,38 @@
+resource "yandex_ydb_database_serverless" "main-db" {
+  name = "main-db"
+
+  serverless_database {
+    enable_throttling_rcu_limit = false
+    provisioned_rcu_limit       = 10
+    storage_size_limit          = 50
+    throttling_rcu_limit        = 0
+  }
+}
+
+resource "yandex_ydb_table" "movie-recognition" {
+  path              = "movie-recognition"
+  connection_string = yandex_ydb_database_serverless.main-db.ydb_full_endpoint
+
+  column {
+    name     = "id"
+    type     = "Int32"
+    not_null = true
+  }
+  column {
+    name     = "video_url"
+    type     = "Utf8"
+    not_null = true
+  }
+  column {
+    name     = "created_at"
+    type     = "Datetime"
+    not_null = true
+  }
+  column {
+    name     = "status"
+    type     = "Utf8"
+    not_null = true
+  }
+
+  primary_key = ["id"]
+}
