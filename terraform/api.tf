4258e48f38a4b0c534119ffbb69b52bd1af24c82
Alexander Lipatov <lipatov@mindbox.cloud>
2024-03-11 22:39:37 +0300

    fix

--- terraform/api.tf
+++ terraform/api.tf
@@ -1,10 +1,10 @@
 resource "yandex_iam_service_account" "api-sa" {
-  name        = "api-sa"
+  name = "api-sa"
 }
 
 resource "yandex_resourcemanager_folder_iam_member" "api-sa-ymq-writer" {
   folder_id = var.folder_id
-  role      = "writerymq.writer"
+  role      = "ymq.writer"
   member    = "serviceAccount:${yandex_iam_service_account.api-sa.id}"
 }
 
@@ -26,10 +26,10 @@ resource "yandex_serverless_container" "test-container" {
   memory             = 128
   service_account_id = yandex_iam_service_account.api-sa.id
   image {
-    url = "cr.yandex/${var.api_registry}/${var.api_repository}:${var.github_sha}"
+    url         = "cr.yandex/${var.api_registry}/${var.api_repository}:${var.github_sha}"
     environment = {
-      YDB_ENDPOINT      = "grpcs://${yandex_ydb_database_serverless.main-db.ydb_api_endpoint}"
-      YDB_DATABASE_PATH = yandex_ydb_database_serverless.main-db.database_path
+      YDB_ENDPOINT          = "grpcs://${yandex_ydb_database_serverless.main-db.ydb_api_endpoint}"
+      YDB_DATABASE_PATH     = yandex_ydb_database_serverless.main-db.database_path
       AWS_ACCESS_KEY_ID     = yandex_iam_service_account_static_access_key.api-sa-key.access_key
       AWS_SECRET_ACCESS_KEY = yandex_iam_service_account_static_access_key.api-sa-key.secret_key
       AWS_DEFAULT_REGION    = "ru-central1"
