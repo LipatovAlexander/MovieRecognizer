e4bf675257fec653a92dc36a5ef080d57736c460
Alexander Lipatov <lipatov@mindbox.cloud>
2024-03-24 17:35:02 +0300

    fix

--- yandex-reverse-image-search/src/function.js
+++ yandex-reverse-image-search/src/function.js
@@ -1,8 +1,9 @@
 import search from "./search.js";
 
 const handler = async function (event, context) {
-  console.log(event);
-  const body = JSON.parse(event.body);
+  const bodyJson = Buffer.from(event.body, "base64").toString("utf8");
+  console.log(bodyJson);
+  const body = JSON.parse(bodyJson);
   const imageUrl = body.imageUrl;
 
   const result = await search(imageUrl);
