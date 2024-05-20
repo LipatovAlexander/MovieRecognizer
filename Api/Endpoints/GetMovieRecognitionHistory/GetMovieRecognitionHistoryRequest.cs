12c72427c72a244061bfd542c10d6bd7ff14a4e9
Alexander Lipatov <lipatov@mindbox.cloud>
2024-05-20 22:53:19 +0300

    .

--- Api/Endpoints/GetMovieRecognitionHistory/GetMovieRecognitionHistoryRequest.cs
+++ Api/Endpoints/GetMovieRecognitionHistory/GetMovieRecognitionHistoryRequest.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace Api.Endpoints.GetMovieRecognitionHistory;
 
 public class GetMovieRecognitionHistoryRequest
 {
+    [FromQuery(Name = "userId")]
     public required Guid UserId { get; set; }
 }
